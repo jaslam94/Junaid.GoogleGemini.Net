e83cf0afeb9dabea3aef7426f45b7cf6c4848737
Junaid <33651074+jaslam94@users.noreply.github.com>
2023-12-25 20:43:53 +0500

    added chat service

---
+++ Junaid.GoogleGemini.Net/Models/Requests/MessageObject.cs
@@ -0,0 +1,30 @@
+﻿namespace Junaid.GoogleGemini.Net.Models.Requests
+{
+    public class MessageObject
+    {
+        private string role;
+        private string text;
+
+        public string Role
+        {
+            get => role;
+            private set => role = value is "model" or "user"
+                ? value
+                : throw new ArgumentException("Value cannot be other than 'model' or 'user'.");
+        }
+
+        public string Text
+        {
+            get => text;
+            private set => text = !string.IsNullOrWhiteSpace(value)
+                ? value
+                : throw new ArgumentException("Text cannot be empty.");
+        }
+
+        public MessageObject(string role, string text)
+        {
+            Role = role;
+            Text = text;
+        }
+    }
+}
