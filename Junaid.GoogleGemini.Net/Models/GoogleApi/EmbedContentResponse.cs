29989ce8b397a9d83e6ea27fd6a161efa9014783
Junaid <33651074+jaslam94@users.noreply.github.com>
2023-12-31 19:19:03 +0500

    added embedding service and models

---
+++ Junaid.GoogleGemini.Net/Models/GoogleApi/EmbedContentResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Junaid.GoogleGemini.Net.Models.GoogleApi
+{
+    public class EmbedContentResponse
+    {
+        public Embedding embedding { get; set; }
+    }
+
+    public class Embedding
+    {
+        public float[] values { get; set; }
+    }
+}
