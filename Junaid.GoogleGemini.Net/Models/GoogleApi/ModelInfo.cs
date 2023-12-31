97ce344ea0ebba3b8395aca483489d9579e5c074
Junaid <33651074+jaslam94@users.noreply.github.com>
2023-12-31 18:36:00 +0500

    added model info service

---
+++ Junaid.GoogleGemini.Net/Models/GoogleApi/ModelInfo.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// 
+/// </summary>
+public class ModelInfo
+{
+    public string name { get; set; }
+    public string version { get; set; }
+    public string displayName { get; set; }
+    public string description { get; set; }
+    public int inputTokenLimit { get; set; }
+    public int outputTokenLimit { get; set; }
+    public string[] supportedGenerationMethods { get; set; }
+    public float temperature { get; set; }
+    public int topP { get; set; }
+    public int topK { get; set; }
+}
---
+++ Junaid.GoogleGemini.Net/Services/ModelInfoService.cs
@@ -0,0 +1,48 @@
+﻿using Junaid.GoogleGemini.Net.Infrastructure;
+
+namespace Junaid.GoogleGemini.Net.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ModelInfoService
+    {
+        private readonly IGeminiClient GeminiClient;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ModelInfoService()
+        {
+            GeminiClient = GeminiConfiguration.GeminiClient;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="geminiClient"></param>
+        public ModelInfoService(GeminiClient geminiClient)
+        {
+            GeminiClient = geminiClient;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<ModelInfo>> ListModelsAsync()
+        {
+            return await GeminiClient.GetAsync<List<ModelInfo>>($"/v1beta/models");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<ModelInfo> GetModelAsync(string name)
+        {
+            return await GeminiClient.GetAsync<ModelInfo>($"/v1beta/models/{name}");
+        }
+    }
+}
